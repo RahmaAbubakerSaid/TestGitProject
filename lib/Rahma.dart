853356109fa90aa1abdfe6048804bea0d61ca18e
Rahma <itsrahmasaid.official@gmail.com>
2022-10-01 20:09:52 +0200

    Edit

--- lib/Rahma.dart
+++ lib/Rahma.dart
@@ -4,7 +4,9 @@ class Rahma extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Hi"),
+      ),
     );
   }
 }
